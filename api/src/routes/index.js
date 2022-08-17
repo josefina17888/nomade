c2c97d2478bf030e8756ea262474bd760409db44
Josefina Garcia Berro <josegarciaberro@gmail.com>
2022-08-17 01:11:21 -0300

    postsReviewsCorregido

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -11,9 +11,11 @@ router.use(bodyParser.urlencoded({ extended: true }))
 const hostRoute = require ('../routes/host/host');
 const bookingRoute = require ('../routes/booking/booking');
 const guestReviewRoute = require ('../routes/guestReview/guestReview.js')
+const lodgingReviewRoute = require('../routes/lodgingReview/lodgingReview')
 
 router.use("/api/host", hostRoute);
 router.use("/api/booking", bookingRoute); 
 router.use("/api/guestReview", guestReviewRoute);
+router.use("/api/lodgingReview", lodgingReviewRoute);
 
 module.exports = router;
