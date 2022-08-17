d717d4791e261edb28d2a9cde624a79de58d97bf
Yulissa Martínez <yulissa.mtzd@gmail.com>
2022-08-16 20:16:39 -0500

    Add ruta POST newBooking sin relacion

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const router = Router();
+var bodyParser = require('body-parser')
+router.use(bodyParser.json()) 
+router.use(bodyParser.urlencoded({ extended: true }))
 
-/* router.get('/', (req, res) =>{
-  res.send('Hello world!')
-}) */
 const hostRoute = require ('../routes/host/host');
 const bookingRoute = require ('../routes/booking/booking');
 
