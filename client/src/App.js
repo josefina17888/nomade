d7a106ede55a62931c10c058f3862a58c8824764
Velgrab <velgrab@gmail.com>
2022-08-18 13:15:11 -0400

    Update Front-End Components, Map, UserLogin, redux and dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,21 @@
+import React from 'react';
 import './App.css';
+import { Route, BrowserRouter, Switch } from 'react-router-dom'; 
+import GoogleMaps from './components/GoogleMaps/GoogleMaps';
+import Home from './components/Home/Home';
+import FormUser from './components/FormUser/FormUser';
 
 function App() {
   return (
-   <div></div>
+   <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/map" component={GoogleMaps} />
+          <Route path="/login" component={FormUser} />
+        </Switch>
+      </BrowserRouter>
+   </div>
   );
 }
 
