62480d07869756a2954e92e40a4a7f077de5cc0c
Josefina Garcia Berro <josegarciaberro@gmail.com>
2022-08-18 13:29:35 -0300

    rutas de reviews actualizadas

--- api/src/routes/guestReview/guestReview.js
+++ api/src/routes/guestReview/guestReview.js
@@ -2,8 +2,10 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 const guestReview = require("../../models/GuestReview");
+const mongoose = require("mongoose");
+const toId = mongoose.Types.ObjectId;
 
-router.post("/", async (req, res) => {
+router.post("/:hostId/:guestId", async (req, res) => {
     let {rating, comments} = req.body;
     if (!rating || !comments){
         return res.status(400).send({message: 'Rating and comments are required'})
@@ -13,8 +15,10 @@ router.post("/", async (req, res) => {
     }
     else { 
         try {
-            const guestRevs = new guestReview(req.body);
-            await guestRevs.save();
+            const guestRevs = await guestReview.create(req.body);
+            guestRevs.hostId = toId(req.params.hostId);
+            guestRevs.guestId = toId(req.params.guestId);
+            guestRevs.save();
             return res.status(200).json(guestRevs);
         } catch (error){console.log(error)}
     }
