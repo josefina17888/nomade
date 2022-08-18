81af032fd776d7cdd6a90f9c0004d702deb19aeb
Velgrab <velgrab@gmail.com>
2022-08-18 13:30:08 -0400

    Update extension js in files

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom'; 
-import GoogleMaps from './components/GoogleMaps/GoogleMaps';
-import Home from './components/Home/Home';
-import FormUser from './components/FormUser/FormUser';
+import GoogleMaps from './components/GoogleMaps/GoogleMaps.js';
+import Home from './components/Home/Home.js';
+import FormUser from './components/FormGuest/FormGuest.js';
 
 function App() {
   return (
