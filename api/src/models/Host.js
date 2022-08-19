b84817b01c08cc698f203819df022e6372eb1f27
Josefina Garcia Berro <josegarciaberro@gmail.com>
2022-08-19 09:35:22 -0300

    conflictos resueltos en modelo host

--- api/src/models/Host.js
+++ api/src/models/Host.js
@@ -2,36 +2,12 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const hostSchema = new Schema({
-<<<<<<< HEAD
-  name: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  cellPhone: {
-    type: String,
-  },
-  dni: String,
-  country: String,
-  birthDate: Date,
-  picture: String,
-  Lodging: [
-    {
-      type: Schema.ObjectId,
-      ref: "lodging",
-    },
-  ],
 
-=======
   guestId: {type: mongoose.Types.ObjectId, ref: "Guest"},
   dni: { type: String, required: true },
   hostDniPicture: { type: String },
   lodgingId: [{type: mongoose.Types.ObjectId, ref: "Lodging"}], 
->>>>>>> 0cf9eeb0e5207b709a95dfe466b7516c0405008a
+
 });
 
 hostSchema.methods.setImgUrl = function setImgUrl (filename) {
