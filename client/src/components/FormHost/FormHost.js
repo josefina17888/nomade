0081daa4875177b128ff8e3627287e0e5cc189c9
Nabil <nallis014@gmail.com>
2022-08-19 18:52:26 -0300

    Actualizacion Form Host

--- client/src/components/FormHost/FormHost.js
+++ client/src/components/FormHost/FormHost.js
@@ -1,14 +1,7 @@
 import React from 'react'
-
-export default function FormHost() {
-  return (
-    <div>FormHost</div>
-  )
-}
-import React from 'react'
 import {useState} from 'react'
-import {useHistory} from 'react-router-dom'
-import{useDispatch, use} from 'react-redux'
+import {useHistory, } from 'react-router-dom'
+import{useDispatch} from 'react-redux'
 import { postHost } from "../../Redux/Actions";
 import estilos from './FormHost.module.css'
 
@@ -17,60 +10,24 @@ export default function FormHost() {
   const history = useHistory()
 
   const [input,setInput] = useState({
-    name : '',
-    lastName: '',
-    email: '',
-    cellPhone: '',
     dni: '',
-    country: '',
-    birthDate: '',
-    photo:'',
+    hostDniPicture:'',
 })
 
 
-function handleName(e){
-  setInput({
-      ...input,
-      [e.target.name]: e.target.value
-  })
-}
-function handleLastName(e){
-  setInput({
-      ...input,
-      [e.target.name]: e.target.value
-  })
-}
-function handleEmail(e){
-  setInput({
-      ...input,
-      [e.target.name]: e.target.value
-  })
-}
 
-function handleCellPhone(e){
-  setInput({
-      ...input,
-      [e.target.name]: e.target.value
-  })
-}
+
+
+
+
 function handleDni(e){
   setInput({
       ...input,
       [e.target.name]: e.target.value
   })
 }
-function handleCountry(e){
-  setInput({
-      ...input,
-      [e.target.name]: e.target.value
-  })
-}
-function handleBirthDate(e){
-  setInput({
-      ...input,
-      [e.target.name]: e.target.value
-  })
-}
+
+
 function handlePhoto(e){
   setInput({
       ...input,
@@ -91,42 +48,7 @@ function handleSubmit(e){
       {/* <form onSubmit={(e)=>handleSubmit(e)} > */}
 
       <form action='http://localhost:3001/api/host' method="POST" encType="multipart/form-data" onSubmit={(e)=>handleSubmit(e)}>
-        <label>Nombre:</label>
-        <input 
-        type="text"
-        name="name" 
-        value={input.name}
-        onChange={handleName}
-        placeholder="Nombre"
-        required
-        />
-        <label>Apellido:</label>
-        <input 
-        type="text"
-        name= "lastName"
-        value={input.lastName}
-        onChange={handleLastName}
-        placeholder="Apellido"
-        required
-        />
-        <label>Email:</label>
-        <input 
-        type="text"
-        name= "email"
-        value={input.email}
-        onChange={handleEmail}
-        placeholder="Email"
-        required
-        />
-        <label>Telefono:</label>
-        <input 
-        type="text" 
-        name="cellPhone"
-        value={input.cellPhone}
-        onChange={handleCellPhone}
-        placeholder="Telefono"
-        required
-        />
+       
         <label>DNI:</label>
         <input 
         type="text" 
@@ -136,25 +58,10 @@ function handleSubmit(e){
         placeholder="DNI"
         required
         />
-        <label>Pais:</label>
-        <input 
-        type="text"
-        name="country" 
-        onChange={handleCountry}
-        value={input.country}
-        placeholder="Pais"
-        />
-        <label>Fecha de Nacimiento:</label>
-        <input 
-        type="date" 
-        name="birthDate"
-        onChange={handleBirthDate}
-        value={input.birthDate}
-        placeholder="Fecha de Nacimiento"
-        />
+        
         <label>Foto:</label>
         <input 
-        name="photo"
+        name="hostDniPicture"
         type="file"
         onChange={handlePhoto}
         />
