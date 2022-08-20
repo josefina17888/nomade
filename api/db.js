fb51b243fd35d8c993342f57c93198b4113547da
Josefina Garcia Berro <98540113+josefina17888@users.noreply.github.com>
2022-08-20 19:17:40 -0300

    Update db.js
    quedó db generica

--- api/db.js
+++ api/db.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-// const uri= `mongodb+srv://nabil:1234@cluster0.7vhfvxb.mongodb.net/?retryWrites=true&w=majority`
+
 mongoose.connect(uri,
     (err)=>{
     if(err){
