0ba7052818163731b3fa5b1dbc05ddb972cd23ac
Velgrab <velgrab@gmail.com>
2022-08-21 17:49:11 -0400

    Update in route Login

--- api/src/routes/login/login.js
+++ api/src/routes/login/login.js
@@ -25,26 +25,3 @@ router.post("/", async (req, res) => {
 
 
 module.exports = router
-
-
-// try {
-//   const user = await Guest.findOne({
-//     email: req.body.email,
-//     password: req.body.password,
-//   });
-
-//   if (user) {
-//     const token = jwt.sign(
-//       {
-//         email: user.email,
-//         name: user.name,
-//       }, process.env.SECURE_TOKEN, { expiresIn: "7d" }
-//     );
-
-//     return res.json({ status: "ok", user: token });
-//   } else {
-//     return res.json({ status: "error", user: false });
-//   }
-// } catch (err) {
-//   res.status(500).json(err);
-// }
