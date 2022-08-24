b41fcc65b7048c1b1a32c49eee85f6d8244d18eb
Nabil <nallis014@gmail.com>
2022-08-24 11:51:03 -0300

    Formulario controlado de Guest

--- client/src/components/FormGuest/FormGuest.js
+++ client/src/components/FormGuest/FormGuest.js
@@ -7,12 +7,11 @@ import {getGuestByEmail} from '../../Redux/Actions'
 import {IoCheckmarkCircleOutline, IoCloseCircleOutline} from 'react-icons/io5'
 import style from "./FormUser.module.css";
 
-
 export default function FormUser() {
   const dispatch= useDispatch()
   const history = useHistory()
- 
-  const [error,setError] =useState(0)
+  const [errors,setErrors] =useState({})
+  const [errorButton, setErrorButton]= useState(true)
   const [input, setInput] = useState({
     username: "",
     name: "",
@@ -56,18 +55,34 @@ const guestByEmail = useSelector((state)=>state.duplicate)
 //   }
 //   console.log(i[email])
 // }
-// console.log(emailRegistrado)
-
-
-
-  function handleChange(e){
-    setInput({
-        ...input,
-        [e.target.name]: e.target.files,
-        [e.target.name] : e.target.value,
-    })
+function handleChange(e){
+  setInput({
+    ...input,
+    [e.target.name]: e.target.files,
+    [e.target.name] : e.target.value,
+  })
+  // let mensaje = true
+    // setErrors(validate({
+    //   ...input,
+    //   [e.target.name]: e.target.value
+    // }))
+    // if(Object.keys(errors).length !==0){
+    //   setErrorButton(true)
+    // }else{
+    //   setErrorButton(false)
+    // }
+    // setNotRequired(required({
+    //   ...input,
+    //   [e.target.name] : e.target.value,
+    // }))
+    // if(Object.keys(required.length !==0)){
+    //    mensaje = true
+    // }else{
+    //    mensaje = false
+    // }
     dispatch(getGuestByEmail(e.target.value))
 }
+
   return (
     <div className={style.containerUser}>
       <form action="http://localhost:3001/api/guest"  method="POST" encType="multipart/form-data" >
@@ -75,28 +90,24 @@ const guestByEmail = useSelector((state)=>state.duplicate)
       <h1 className={style.title}>Registrate!</h1>
       <div className={style.containerForm}>
         <input
-          // className={style.inputEmail}
-          type="text"
-          name ="username"
-          value={input.username}
-          placeholder="Nombre de usuario"
-          onChange={handleChange}
-        />
-         <input
-          // className={style.inputEmail}
           type="text"
           name ="name"
           value={input.name}
           placeholder="Nombre"
           onChange={handleChange}
+          title="Nombre solo acepta letras y espacios en blanco"
+          pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$"
+          required
         />
         <input
-          // className={style.inputPassword}
           type="text"
           name ="lastname"
           value={input.lastname}
           placeholder="Apellido"
           onChange={handleChange}
+          title="Apellido solo acepta letras y espacios en blanco"
+          pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$"
+          required
         />
          <input
           type="text"
@@ -104,42 +115,49 @@ const guestByEmail = useSelector((state)=>state.duplicate)
           value={input.email}
           placeholder="Email"
           onChange={handleChange}
+          title="Email incorrecto"
+          pattern="^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$"
+          required
           />
           {guestByEmail.length === 0 ? <IoCheckmarkCircleOutline/>: <IoCloseCircleOutline/>}
          <input
-          // className={style.inputEmail}
           type="password"
           name ="password"
           value={input.password}
           placeholder="contraseña"
           onChange={handleChange}
+          title="La contraseña debe contener solo letras [a-z], números[0-9] o caracteres especiales (@#$%&)"
+          pattern="^[a-z0-9\.@#\$%&]+$"
+          required
         />
         <input
-          // className={style.inputEmail}
           type="text"
           name ="dni"
           value={input.dni}
           placeholder="dni"
           onChange={handleChange}
+          title="El DNI debe contener números positivos"
+          pattern="^\d+$"
         />
         <input
-          // className={style.inputPassword}
           type="text"
           name ="cellPhone"
           value={input.cellPhone}
           placeholder="Celular"
           onChange={handleChange}
+          title=""
+          pattern="^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$"
         />
          <input
-          // className={style.inputEmail}
           type="text"
           name ="country"
           value={input.country}
           placeholder="pais"
           onChange={handleChange}
+          title="El país solo acepta letras y espacios en blanco"
+          pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$"
         />
          <input
-          // className={style.inputEmail}
           type="date"
           name ="birthDate"
           value={input.birthDate}
@@ -147,7 +165,6 @@ const guestByEmail = useSelector((state)=>state.duplicate)
           onChange={handleChange}
         />
         <input
-          // className={style.inputPassword}
           type="file"
           name ="picture"
           value={input.picture}
@@ -156,13 +173,10 @@ const guestByEmail = useSelector((state)=>state.duplicate)
         />
       </div>
       <button className={style.button} type="submit">
-        Iniciar Sesion
+        Registrarme
       </button>
   </form>
     </div>
   );
-
-
-
 }
 
