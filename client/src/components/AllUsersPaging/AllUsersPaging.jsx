e5f6aa478315a33f0a60ae4a279a325b78b9bfe2
BruLau <brunomlauricella@gmail.com>
2022-09-03 21:43:25 -0300

    correcciones

--- client/src/components/AllUsersPaging/AllUsersPaging.jsx
+++ client/src/components/AllUsersPaging/AllUsersPaging.jsx
@@ -3,6 +3,9 @@ import styles from "../AllCardsPaging/AllCardsPaging.module.css"
 
 export default function AllUsersPaging ({lodgingPerPage, stateLodgings, paging}){
     const pageNumbers = [];
+    console.log(stateLodgings)
+    console.log(lodgingPerPage)
+    console.log(paging)
     for (let i = 0; i < Math.ceil(stateLodgings/lodgingPerPage); i++){
         //Math.ceil redondea todos mis lodgings sobre el Q de lodgings que quiero por página
         pageNumbers.push(i+1)
