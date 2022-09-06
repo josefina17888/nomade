5bcafe517b4d83fb1b9e3b89e29bab4cde409689
BruLau <brunomlauricella@gmail.com>
2022-09-05 21:08:43 -0300

    agrega entre 3 a 10 imagenes

--- client/src/components/FormLodging/FormLodging.js
+++ client/src/components/FormLodging/FormLodging.js
@@ -110,7 +110,6 @@ export default function FormLodging() {
   function handleDelete(){
     document.getElementById("file").click()
   }
-  
   function handleChange(e){
     if(e.target.name!== "picture")
     {  
@@ -124,17 +123,16 @@ export default function FormLodging() {
   }))
 }
 else{
-  
+ 
+  let imgs=Object.entries(e.target.files).length
   if(document.getElementById("imgPreview0"))
   {
-    for(let i = 0; i<3 ; i++)
-    {
-      if(document.getElementById("imgPreview" + i))
-      {
+    let i= 0
+    while(document.getElementById("imgPreview" + i)){
         document.getElementById("imgPreview" + i).remove();
-      }
-    }
+      i++}
   }
+  
   for(let i= 0; i<e.target.files.length;i++ )
   {
     let  reader = new FileReader()
@@ -158,7 +156,6 @@ else{
     boton.innerHTML = "Cambiar seleccion";
     buttonDelet.append(boton)
   }
-  let imgs = Object.entries(e.target.files).length
   setInput({
     ...input,
     [e.target.name] : e.target.value
--- client/src/components/FormLodging/validation.js
+++ client/src/components/FormLodging/validation.js
@@ -48,9 +48,13 @@ export default function validate (input)
         else if (input.picture < 1){
             errors.picture = "Debe completar el campo imagen. recuerde subir 3 fotos";
         }
-        if (typeof input.picture !== 'string' && input.picture !== 3){
-            errors.picture = "Debes subir 3 imagenes";
+        if (typeof input.picture !== 'string' && input.picture < 3){
+            errors.picture = "Debes subir minimo 3 imagenes";
         }
+        else if(input.picture > 10){
+            errors.picture = "Puedes subir como maximo 10 imagenes";
+        }
+        
         if(!input.latitud){
             errors.latitud = "Debes verificar la direccion";
         }
